2006d6d33f7811c67e95c15521a0a3e5a358df77
AccRPA <accrpaw10@gmail.com>
2023-06-04 23:30:16 +0200

    Second version

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+    selectHomeMenu();
+    onMenuClick();
+});
+
+function selectHomeMenu(){
     $('.header li a:first').addClass('active');
-    $('.header li a').on('click', function(){
-        $('.header li a').removeClass('active');
+}
+
+function onMenuClick(){
+    const menu_links = $('.header li a');
+    menu_links.on('click', function(){
+        menu_links.removeClass('active');
         $(this).addClass('active');
     });
-});
+}
