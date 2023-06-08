72c90a107c2e9c127de590790d6775b2f70eba41
AccRPA <accrpaw10@gmail.com>
2023-06-08 11:34:46 +0200

    Experriment setting the width of the menu according to the scrollbar

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     selectHomeMenu();
     onMenuClick();
+    onScroll();
 });
 
 function selectHomeMenu(){
@@ -13,4 +14,38 @@ function onMenuClick(){
         menu_links.removeClass('active');
         $(this).addClass('active');
     });
+}
+
+function onScroll(){
+    $(document).on('scroll', function(){
+        const array = [];
+        $('section#main > div').each(function(){
+            array.push($(this));
+        })
+
+        const scrollTop = document.scrollingElement.scrollTop;
+        
+        let find = false;
+        let sumHeight = 0;
+        for (let i = 0; i < array.length && !find; i++){
+            const id = array[i].prop('id');
+            const height = array[i].height();
+            sumHeight = sumHeight + height;
+            
+            const link = $('a[href="#' + id +'"]');
+            const span = $('a[href="#' + id +'"] > span');
+            const coordinate = scrollTop - sumHeight;
+            if (coordinate < 0){
+                // I can see this div
+                const widthPercent = ((-coordinate) * 100) / height;
+                //console.log(`index: ${i}, sumHeight: ${sumHeight}, scrollTop: ${scrollTop}, widthPercent: ${widthPercent}`);
+                link.addClass('active');
+                span.css('width', widthPercent + '%');
+                find = true;
+            }else{
+                link.removeClass('active');
+                span.css('width', '0%');
+            }
+        }
+    })
 }
