5d66adf0494652702060deafc6b221339a970ee8
AccRPA <accrpaw10@gmail.com>
2023-06-08 12:16:03 +0200

    Some comments

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function selectHomeMenu(){
 function onMenuClick(){
     const menu_links = $('.header li a');
     menu_links.on('click', function(){
+        // for avoiding to execute the scroll code plus the scroll smooth movement when clicking on a link
         clickNoScroll = true;
         menu_links.removeClass('active');
         $(this).addClass('active');
@@ -22,6 +23,7 @@ function onMenuClick(){
 $(window).scroll(function() {
     clearTimeout($.data(this, 'scrollTimer'));
     $.data(this, 'scrollTimer', setTimeout(function() {
+        // once the scroll movement is finished, reset the variable 
         clickNoScroll = false;
         console.log("Haven't scrolled in 250ms!");
     }, 250));
@@ -29,7 +31,9 @@ $(window).scroll(function() {
 
 function onScroll(){
     $(document).on('scroll', function(){
+        // if the user didn't click on the menu, excecute this scroll code
         if (!clickNoScroll){
+            // array with all the sections
             const array = [];
             $('section#main > div').each(function(){
                 array.push($(this));
@@ -39,13 +43,15 @@ function onScroll(){
 
             let find = false;        
             for (let i = 0; i < array.length && !find; i++){
+                // checking in which section is the scroll
                 const id = array[i].prop('id');
                 const link = $('a[href="#' + id +'"]');
-                const height = array[i].height();                        
+                const height = array[i].height();               
                 const min = i * height;
                 const max = (min + height);
                 if (scrollTop >= min && scrollTop < max){
                     link.addClass('active');
+                    // changing the location in case of refreshing the page
                     window.location.href = '#' + id;
                 }else{
                     link.removeClass('active');
